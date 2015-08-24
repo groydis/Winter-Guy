dc9563fc46ad5101eef334ba1e3881d7d2ac2630
Greyden Scott <greydenscott@gmail.com>
2015-08-24 22:03:30 +1000

    Game loading board

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,30 +2,34 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
-
-	public BoardManager boardScript;
-
-	private int level = 3;
-
-	void Awake ()
-	{
-		boardScript = GetComponent<BoardManager> ();
-		InitGame ();
-	}
-
-
-	void InitGame() 
-	{
-		boardScript.SetupScene (level);
-	}
-
-	// Use this for initialization
-	void Start () {
 	
+	public static GameManager instance = null;
+	
+	public BoardManager boardScript;
+	
+	private int level = 3;
+	
+	
+	// Use this for initialization
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject);
+		}
+
+		DontDestroyOnLoad (gameObject);
+		
+		boardScript = GetComponent<BoardManager>();
+		InitGame (); 
+	}
+	
+	void InitGame() {
+		boardScript.SetupScene(level);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
