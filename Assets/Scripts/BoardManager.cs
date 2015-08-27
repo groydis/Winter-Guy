ba97803f31efe7532f4995311f93202d6b4ae1ce
Greyden Scott <greydenscott@gmail.com>
2015-08-27 14:58:53 +1000

    Revert "Semi Operational State"
    This reverts commit 963ce893d17c0def0938b0448f640647fb8337c1.
    
    Conflicts:
    	Library/CurrentLayout.dwlt
    	Library/assetDatabase3
    	Library/expandedItems
    	Library/metadata/00/00000000000000004000000000000000
    	Winter-Guy-csharp.sln
    	Winter-Guy.sln
    	Winter-Guy.userprefs

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -69,7 +69,7 @@ public class BoardManager : MonoBehaviour {
 		LayoutObjectAtRandom(wallTiles, wallCount.minimum, wallCount.maximum);
 		LayoutObjectAtRandom(foodTiles, foodCount.minimum, foodCount.maximum);
 		int enemyCount = (int)Mathf.Log(level, 2f);
-		LayoutObjectAtRandom(enemyTiles, enemyCount, enemyCount);
+		//LayoutObjectAtRandom(enemyTiles, enemyCount, enemyCount);
 		Instantiate(exit, new Vector3(columns -1, rows -1, 0f), Quaternion.identity);
 	}
 }
